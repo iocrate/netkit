783255e5899f330ef780df0e8882b5679608967b
tulayang <itulayangi@outlook.com>
2020-04-29 18:14:45 +0800

    improve comments of http.writer

--- doc/zh/code/netkit/http/writer.nim
+++ doc/zh/code/netkit/http/writer.nim
@@ -4,16 +4,15 @@
 #    See the file "LICENSE", included in this
 #    distribution, for details about the copyright.
 
-## This module provides an abstraction of write operations related to HTTP.
+## 这个模块定义 HTTP 相关的写操作的抽象。
 ## 
-## Overview
+## 概述
 ## ========================
 ## 
-## A server writes a response to a client, and a client writes a request to a server.
+## 服务器将响应发给客户端，客户端将请求发给服务器。
 ## 
-## ``HttpWriter`` is a base object for write operations, ``ServerResponse`` and ``ClientRequest`` 
-## inherit from it. ``ServerResponse`` represents a response from a server, and ``ClientRequest``
-## represents a request from a client.
+## ``HttpWriter`` 是写操作的基对象， ``ServerResponse`` 和 ``ClientRequest`` 继承自该对象。 
+## ``ServerResponse`` 表示服务器发出的响应， ``ClientRequest`` 表示客户端发出的请求。
 
 import strutils
 import asyncdispatch
@@ -26,55 +25,52 @@ import netkit/http/header
 import netkit/http/connection
 
 type
-  HttpWriter* = ref object of RootObj ## An abstraction of write operations related to HTTP.
+  HttpWriter* = ref object of RootObj ## 表示 HTTP 相关的写操作。
     conn: HttpConnection
     lock: AsyncLock
     onEnd: proc () {.gcsafe, closure.}
     writable: bool
 
-  ServerResponse* = ref object of HttpWriter ## Represents a response from a server.
-  ClientRequest* = ref object of HttpWriter ## Represents a request from a client.
+  ServerResponse* = ref object of HttpWriter ## 表示服务器发出的响应。
+  ClientRequest* = ref object of HttpWriter ## 表示客户端发出的请求。
 
 proc newServerResponse*(conn: HttpConnection, onEnd: proc () {.gcsafe, closure.}): ServerResponse = discard
-  ## Creates a new ``ServerResponse``.
+  ## 创建一个新的 ``ServerResponse`` 。
   
 proc newClientRequest*(conn: HttpConnection, onEnd: proc () {.gcsafe, closure.}): ClientRequest = discard
-  ## Creates a new ``ClientRequest``.
+  ## 创建一个新的 ``ClientRequest`` 。
 
 proc ended*(writer: HttpWriter): bool {.inline.} = discard
-  ## Returns ``true`` if the underlying connection has been closed or writer has been shut down.
+  ## 如果底部连接已断开或写端已经关闭，则返回 ``true`` 。
 
 proc write*(writer: HttpWriter, buf: pointer, size: Natural): Future[void] {.async.} = discard
-  ## Writes ``size`` bytes from ``buf`` to the writer.
+  ## 从 ``buf`` 写入 ``size`` 字节的数据。
   ## 
-  ## If a system error occurs during writing, an ``OsError``  will be raised. If the connection is  
-  ## disconnected before successful writing or the writer has been shut down, a ``ReadAbortedError`` will be raised.
+  ## 如果写过程中出现系统错误，则会触发 ``OSError`` 异常；如果在成功写之前连接断开或者写端已经关闭，则会触发 ``WriteAbortedError`` 异常。
 
 proc write*(writer: HttpWriter, data: string): Future[void] {.async.} = discard
-  ## Writes a string to the writer.
+  ## 写入一个字符串。
   ## 
-  ## If a system error occurs during writing, an ``OsError``  will be raised. If the connection is  
-  ## disconnected before successful writing or the writer has been shut down, a ``ReadAbortedError`` will be raised.
+  ## 如果写过程中出现系统错误，则会触发 ``OSError`` 异常；如果在成功写之前连接断开或者写端已经关闭，则会触发 ``WriteAbortedError`` 异常。
 
 proc write*(
   writer: HttpWriter, 
   statusCode: HttpCode,
   fields: openArray[tuple[name: string, value: string]]
 ): Future[void]  = discard
-  ## Writes a message header to the writer.
+  ## 写入一个消息头。
   ## 
-  ## If a system error occurs during writing, an ``OsError``  will be raised. If the connection is  
-  ## disconnected before successful writing or the writer has been shut down, a ``ReadAbortedError`` will be raised.
+  ## 如果写过程中出现系统错误，则会触发 ``OSError`` 异常；如果在成功写之前连接断开或者写端已经关闭，则会触发 ``WriteAbortedError`` 异常。
 
 proc write*(
   writer: HttpWriter, 
   statusCode: HttpCode,
   fields: openArray[tuple[name: string, value: seq[string]]]
 ): Future[void] = discard
-  ## Writes a message header to the writer.
+  ## 写入一个消息头。
   ## 
-  ## If a system error occurs during writing, an ``OsError``  will be raised. If the connection is  
-  ## disconnected before successful writing or the writer has been shut down, a ``ReadAbortedError`` will be raised.
+  ## 如果写过程中出现系统错误，则会触发 ``OSError`` 异常；如果在成功写之前连接断开或者写端已经关闭，则会触发 ``WriteAbortedError`` 异常。
 
 proc writeEnd*(writer: HttpWriter) = discard
-  ## Shuts down writer. Data is no longer allowed to be written, otherwise an ``WriteAbortedError`` will be raised.
+  ## 关闭写端。之后，不能继续向 ``writer`` 写入数据，否则将引发 ``WriteAbortedError`` 异常。
+  
