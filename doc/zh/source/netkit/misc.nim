0ddabc7019d562e59c1616352fde6e155cdbe3f7
tulayang <itulayangi@outlook.com>
2020-04-29 10:44:25 +0800

    improve comments of misc

--- doc/zh/source/netkit/misc.nim
+++ doc/zh/source/netkit/misc.nim
@@ -4,8 +4,10 @@
 #    See the file "LICENSE", included in this
 #    distribution, for details about the copyright.
 
+## 这个模块包含了一些其他功能，这些功能不属于任何其他模块。
+
 template offset*(p: pointer, n: int): pointer = discard 
-  ## 
+  ## Returns a new pointer, which is offset ``n`` bytes backwards from ``p``.
    
 template checkDefNatural*(value: static[Natural], name: static[string]): untyped = discard
   ## 检查 ``value`` 是否是自然数 (零和正整数) 。 如果不是，则停止编译。 ``name`` 指定其符号名字。 
