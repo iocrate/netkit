a3024fd96089be3ae93eb41bfdb7ad8ba07cd189
tulayang <itulayangi@outlook.com>
2020-04-29 17:03:02 +0800

    improve comments of http.httpmethod

--- doc/zh/code/netkit/http/httpmethod.nim
+++ doc/zh/code/netkit/http/httpmethod.nim
@@ -4,22 +4,20 @@
 #    See the file "LICENSE", included in this
 #    distribution, for details about the copyright.
 
-## This module contains a definition of HTTP request method. 
+## 这个模块包含 HTTP 请求方法的定义。
 ## 
-## Overview
+## 概述
 ## ========================
 ## 
-## HTTP defines methods to indicate the desired action to be performed on the identified resource. What this 
-## resource represents, whether pre-existing data or data that is generated dynamically, depends on the  
-## implementation of the server. Often, the resource corresponds to a file or the output of an executable
-## residing on the server. 
+## HTTP 定义了请求方法，以标识对资源执行的操作。资源代表什么，是预先存在的数据还是动态生成的数据，取决于服务器的实现。
+## 通常，资源与服务器上驻留的文件或可执行文件的输出相对应。
 ## 
 ## .. 
 ## 
-##   See `Hypertext Transfer Protocol <https://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol>`_ for more information.
+##   看看 `Hypertext Transfer Protocol <https://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol>`_ 了解更多。
 
 type
-  HttpMethod* = enum ## HTTP request method. 
+  HttpMethod* = enum ## HTTP 请求方法。
     HttpHead = "HEAD",        
     HttpGet = "GET",         
     HttpPost = "POST",        
@@ -31,9 +29,9 @@ type
     HttpPatch = "PATCH" 
 
 proc parseHttpMethod*(s: string): HttpMethod {.raises: [ValueError].} = discard
-  ## Converts a string to an HTTP request method. A ``ValueError`` is raised when ``s`` is not a valid method.
+  ## 将字符串转换为 HTTP 请求方法。如果 ``s`` 不是有效的请求方法，则会引发 ``ValueError`` 。
   ## 
-  ## Examples:
+  ## 例子：
   ## 
   ## .. code-block::nim
   ## 
