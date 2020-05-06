d0d2633d38451912bee011009566f72549e5f97a
tulayang <itulayangi@outlook.com>
2020-05-06 15:39:34 +0800

    del benchmark dir

--- benchmark/server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('Hello World');
-});
-
-server.listen(8080)
--- benchmark/stdserver.nim
+++
@@ -1,9 +0,0 @@
-import asynchttpserver, asyncdispatch
-
-var server = newAsyncHttpServer()
-
-proc cb(req: Request) {.async.} =
-  # await req.respond(Http200, "")
-  await req.respond(Http200, "Hello World")
-
-waitFor server.serve(Port(8080), cb)
